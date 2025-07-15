ff7dd19cc00b72d418e8cfbf8bb3409fc4e9b395
Mansi Negi <mansinegi135@gmail.com>
2025-07-15 12:28:02 +0530

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -21,21 +21,21 @@ from src.logger import logging
 from src.utils import save_object,evaluate_model
 
 @dataclass 
-class ModelTrainerConfig:
+class MyModelTrainerConfig:
     trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config=ModelTrainerConfig()
+        self.model_trainer_config=MyModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_array,test_array):
+    def initiate_model_trainer(self,train_arr,test_arr):
         try:
             logging.info("Split training and test input data")
             X_train,y_train,X_test,y_test = (
-                train_array[:, :-1],
-                train_array[:, -1],
-                test_array[:, :-1],
-                test_array[:, -1]
+                train_arr[:, :-1],
+                train_arr[:, -1],
+                test_arr[:, :-1],
+                test_arr[:, -1]
             )
 
             models = {
@@ -45,12 +45,41 @@ class ModelTrainer:
                 "Linear Regression": LinearRegression(),
                 "K-Neighbours": KNeighborsRegressor(),
                 "XGBoost": XGBRegressor(),
-                "CatBoost": CatBoostRegressor(verbose=False)
- ,
-                "Adaboost": AdaBoostRegressor(),
+                "CatBoost": CatBoostRegressor(verbose=False) ,
+                "AdaBoost": AdaBoostRegressor(),
+            }
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                },
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbours":{
+                    'n_neighbors':[5,7,9,11]
+                },
+                "XGBoost":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoost":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
             }
 
-            models_report:dict = evaluate_model(X_train = X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            models_report:dict = evaluate_model(X_train = X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,params=params)
 
             best_model_score = max(sorted(models_report.values()))
             best_model_name = list(models_report.keys())[
