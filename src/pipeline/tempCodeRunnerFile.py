aebee405ff0be6b959a3938b787627c2d12e23cc
Mansi Negi <mansinegi135@gmail.com>
2025-07-19 12:11:01 +0530

    Deployment configuration

---
+++ src/pipeline/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+gender: str,
+        race_ethnicity: str,
+        parental_level_of_education,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int
